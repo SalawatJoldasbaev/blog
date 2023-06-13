dc272dc3285a7c83640d652ca691666462a763ed
Salawat Joldasbaev <jsalawatdeveloper@gmail.com>
2023-06-13 17:37:11 +0500

    added like and dislike buttons + logic

--- app/Http/Livewire/UpvoteDownvote.php
+++ app/Http/Livewire/UpvoteDownvote.php
@@ -2,12 +2,69 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class UpvoteDownvote extends Component
 {
-    public function render()
+    public Post $post;
+
+    public function mount(Post $post): void
     {
-        return view('livewire.upvote-downvote');
+        $this->post = $post;
+    }
+
+    public function render(): View
+    {
+        $upvotes = \App\Models\UpvoteDownvote::query()->where('post_id', $this->post->id)
+            ->where('is_upvote', 1)->count();
+        $downvotes = \App\Models\UpvoteDownvote::query()->where('post_id', $this->post->id)
+            ->where('is_upvote', 0)->count();
+        $hasUpvote = null;
+        /** @var User $user */
+        $user = request()->user();
+
+        if ($user) {
+            $model = \App\Models\UpvoteDownvote::query()
+                ->where('post_id', $this->post->id)
+                ->where('user_id', $user->id)->first();
+            if ($model) {
+                $hasUpvote = !!$model->is_upvote;
+            }
+        }
+
+        return view('livewire.upvote-downvote', compact('upvotes', 'downvotes', 'hasUpvote'));
+    }
+
+    public function upvoteDownvote($upvote = true)
+    {
+        /** @var User $user */
+        $user = request()->user();
+        if (!$user) {
+            $this->redirect(route('login'));
+        }
+        if (!$user->hasVerifiedEmail()) {
+            $this->redirect(route('verification.notice'));
+        }
+        $model = \App\Models\UpvoteDownvote::query()
+            ->where('post_id', $this->post->id)
+            ->where('user_id', $user->id)->first();
+
+        if (!$model) {
+            \App\Models\UpvoteDownvote::create([
+                'is_upvote' => $upvote,
+                'post_id' => $this->post->id,
+                'user_id' => $user->id,
+            ]);
+            return;
+        }
+        if ($upvote && $model->is_upvote || !$upvote && !$model->is_upvote) {
+            $model->delete();
+        } else {
+            $model->is_upvote = $upvote;
+            $model->save();
+        }
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationItem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Filament::serving(function () {
+            Filament::registerNavigationItems([
+                NavigationItem::make('Author')
+                    ->url('https://t.me/salwat_me', shouldOpenInNewTab: true)
+                    ->icon('heroicon-o-cursor-click')
+                    ->group('Other')
+                    ->sort(3),
+            ]);
+        });
     }
 }
